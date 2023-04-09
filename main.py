227677710f7247b622b0aab62b661509e2dbaba0
J3ldo <97303347+J3ldo@users.noreply.github.com>
2023-04-09 19:42:04 +0200

    Fixed ratelimit and issue where it isnt buying

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from threading import Thread
 import os
 import uuid
 import time
+import datetime
 
 with open("limiteds.txt", "r") as f:
     limiteds = f.read().replace(" ", "").split(",")
@@ -28,7 +29,7 @@ def get_x_token():
 
 
 def buy(json, itemid, productid):
-    print("Buying limited...")
+    print("Spam buying limited...")
 
     data = {
         "collectibleItemId": itemid,
@@ -38,74 +39,72 @@ def buy(json, itemid, productid):
         "expectedPurchaserType": "User",
         "expectedSellerId": json["creatorTargetId"],
         "expectedSellerType": "User",
-        "idempotencyKey": str(uuid.uuid4()),
+        "idempotencyKey": "random uuid4 string that will be your key or smthn",
         "collectibleProductId": productid
     }
-    bought = r.post(f"https://apis.roblox.com/marketplace-sales/v1/item/{itemid}/purchase-item", json=data,
-        headers={"x-csrf-token": x_token}, cookies={".ROBLOSECURITY": cookie}).json()
 
-    if not bought["purchased"]:
-        print("Failed buying the limited, trying again..")
-        
-        try:        
-            info = r.post("https://catalog.roblox.com/v1/catalog/items/details",
-                      json={"items": [{"itemType": "Asset", "id": int(limited)}]},
-                      headers={"x-csrf-token": x_token}, cookies={".ROBLOSECURITY": cookie}).json()
-        except:
-            print("Ratelimit!")
-            return
+    while 1:
+        data["idempotencyKey"] = str(uuid.uuid4())
+        bought = r.post(f"https://apis.roblox.com/marketplace-sales/v1/item/{itemid}/purchase-item", json=data,
+            headers={"x-csrf-token": x_token}, cookies={".ROBLOSECURITY": cookie})
 
-        if info["data"][0]["unitsAvailableForConsumption"] == 0:
-            print("Couldn't buy the limited in time. Better luck next time.")
-            return
+        if bought.reason == "Too Many Requests":
+            print("Ran into a ratelimit resuming trying again shortly...")
+            time.sleep(0.5)
+            continue
 
         try:
-            productid = r.post("https://apis.roblox.com/marketplace-items/v1/items/details",
-                           json={"itemIds": [info["data"][0]["collectibleItemId"]]},
-                           headers={"x-csrf-token": x_token}, cookies={".ROBLOSECURITY": cookie}).json()[0]["collectibleProductId"]
+            bought = bought.json()
         except:
-            print("Ratelimit Recieved info: ", info)
-            return
-        
-        buy(json, productid, info["data"][0]["collectibleItemId"])
-        return
+            print(bought.reason)
+            print("Json decoder error whilst trying to buy item.")
+            continue
 
-    print(f"Successfully bought the limited! Info: {bought} - {data}")
+        if not bought["purchased"]:
+            print(f"Failed buying the limited, trying again.. Info: {bought} - {data}")
+        else:
+            print(f"Successfully bought the limited! Info: {bought} - {data}")
+
+        try:
+            info = r.post("https://catalog.roblox.com/v1/catalog/items/details",
+                      json={"items": [{"itemType": "Asset", "id": int(limited)}]},
+                      headers={"x-csrf-token": x_token}, cookies={".ROBLOSECURITY": cookie}).json()["data"][0]
+        except:
+            info = {"unitsAvailableForConsumption": 1}
+
+        if info["unitsAvailableForConsumption"] == 0:
+            print("Couldn't buy the limited in time. Better luck next time.")
+            return
 
 
 # Get collectible and product id for all the limiteds.
 Thread(target=get_x_token).start()
 
 print("UGC Sniper made by Jeldo#9587\nDiscord server: https://discord.com/invite/3Uvcf8d9aY")
-time.sleep(5)
 while x_token == "":
     time.sleep(0.01)
 
 # https://apis.roblox.com/marketplace-items/v1/items/details
 # https://catalog.roblox.com/v1/catalog/items/details
 
-cooldown = 60/(35/len(limiteds))
+cooldown = 60/(39/len(limiteds))
 while 1:
     start = time.perf_counter()
 
     for limited in limiteds:
         try:
             info = r.post("https://catalog.roblox.com/v1/catalog/items/details",
-                               json={"items": [{"itemType": "Asset", "id": int(limited)}]},
-                               headers={"x-csrf-token": x_token}, cookies={".ROBLOSECURITY": cookie}).json()['data'][0]
+                           json={"items": [{"itemType": "Asset", "id": int(limited)}]},
+                           headers={"x-csrf-token": x_token}, cookies={".ROBLOSECURITY": cookie}).json()["data"][0]
         except KeyError:
-            print("Ratelimit waiting for minute to end")
+            print("Ratelimit! Waiting for next minute to start")
             time.sleep(60-int(datetime.datetime.now().second))
             continue
-            
+
         if info.get("priceStatus", "") != "Off Sale":
-            try:
-                productid = r.post("https://apis.roblox.com/marketplace-items/v1/items/details",
-                       json={"itemIds": [info["collectibleItemId"]]},
-                       headers={"x-csrf-token": x_token}, cookies={".ROBLOSECURITY": cookie}).json()[0]["collectibleProductId"]
-            except:
-                print("Ratelimit whilst trying to buy. Recieved info: ", info)
-                continue
+            productid = r.post("https://apis.roblox.com/marketplace-items/v1/items/details",
+                   json={"itemIds": [info["collectibleItemId"]]},
+                   headers={"x-csrf-token": x_token}, cookies={".ROBLOSECURITY": cookie}).json()[0]["collectibleProductId"]
 
             buy(info, info["collectibleItemId"], productid)
 
@@ -116,4 +115,4 @@ while 1:
     os.system("cls")
     print("Check done.\n"
           f"Time taken: {round(time.perf_counter()-start, 3)}\n"
-          f"Ideal time: {cooldown}")
+          f"Ideal time: {round(cooldown, 3)}")
