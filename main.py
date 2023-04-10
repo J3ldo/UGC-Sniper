929630aa076367c8ae5d286ee24a8b6ed81a1262
max <93793772+maxhithere@users.noreply.github.com>
2023-04-09 22:08:46 -0400

    proxy support

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 import uuid
 import time
 import datetime
+from itertools import cycle
 
 with open("limiteds.txt", "r") as f:
     limiteds = f.read().replace(" ", "").split(",")
@@ -11,25 +12,30 @@ with open("limiteds.txt", "r") as f:
 with open("cookie.txt", "r") as f:
     cookie = f.read()
 
+with open("proxies.txt", "r") as f:
+    proxies = f.read().splitlines()
+    proxy_pool = cycle(proxies)
+    proxy = next(proxy_pool)
 
-user_id = r.get("https://users.roblox.com/v1/users/authenticated", cookies={".ROBLOSECURITY": cookie}).json()["id"]
+
+user_id = r.get("https://users.roblox.com/v1/users/authenticated", cookies={".ROBLOSECURITY": cookie}, proxies={'http':"http://"+proxy}).json()["id"] #proxies={'http':"http://"+proxy})
 x_token = ""
 def get_x_token():
     global x_token
 
     x_token = r.post("https://auth.roblox.com/v2/logout",
-                     cookies={".ROBLOSECURITY": cookie}).headers["x-csrf-token"]
-    print("Logged in.")
+                     cookies={".ROBLOSECURITY": cookie}, proxies={'http':"http://"+proxy}).headers["x-csrf-token"]
+    
 
     while 1:
         # Gets the x_token every 4 minutes.
         x_token = r.post("https://auth.roblox.com/v2/logout",
-                         cookies={".ROBLOSECURITY": cookie}).headers["x-csrf-token"]
+                         cookies={".ROBLOSECURITY": cookie}, proxies={'http':"http://"+proxy}).headers["x-csrf-token"]
         time.sleep(248)
 
 
 def buy(json, itemid, productid):
-    print("Spam buying limited...")
+    print("BUYING LIMITED: " + itemid)
 
     data = {
         "collectibleItemId": itemid,
@@ -46,10 +52,12 @@ def buy(json, itemid, productid):
     while 1:
         data["idempotencyKey"] = str(uuid.uuid4())
         bought = r.post(f"https://apis.roblox.com/marketplace-sales/v1/item/{itemid}/purchase-item", json=data,
-            headers={"x-csrf-token": x_token}, cookies={".ROBLOSECURITY": cookie})
+            headers={"x-csrf-token": x_token}, cookies={".ROBLOSECURITY": cookie}, proxies={'http':"http://"+proxy})
 
         if bought.reason == "Too Many Requests":
-            print("Ran into a ratelimit resuming trying again shortly...")
+            print("Ran into a ratelimit, switching proxy and trying again.")
+            proxy = next(proxy_pool) # switch proxy
+            print("Proxy: " + proxy)
             time.sleep(0.5)
             continue
 
@@ -67,7 +75,7 @@ def buy(json, itemid, productid):
 
         info = r.post("https://catalog.roblox.com/v1/catalog/items/details",
                       json={"items": [{"itemType": "Asset", "id": int(limited)}]},
-                      headers={"x-csrf-token": x_token}, cookies={".ROBLOSECURITY": cookie})
+                      headers={"x-csrf-token": x_token}, cookies={".ROBLOSECURITY": cookie}, proxies={'http':"http://"+proxy})
         try:
             left = info.json()["data"][0]["unitsAvailableForConsumption"]
         except:
@@ -82,7 +90,7 @@ def buy(json, itemid, productid):
 # Get collectible and product id for all the limiteds.
 Thread(target=get_x_token).start()
 
-print("UGC Sniper made by Jeldo#9587\nDiscord server: https://discord.com/invite/3Uvcf8d9aY")
+print("Starting Sniper")
 while x_token == "":
     time.sleep(0.01)
 
@@ -97,16 +105,19 @@ while 1:
         try:
             info = r.post("https://catalog.roblox.com/v1/catalog/items/details",
                            json={"items": [{"itemType": "Asset", "id": int(limited)}]},
-                           headers={"x-csrf-token": x_token}, cookies={".ROBLOSECURITY": cookie}).json()["data"][0]
+                           headers={"x-csrf-token": x_token}, cookies={".ROBLOSECURITY": cookie}, proxies={'http':"http://"+proxy}).json()["data"][0]
         except KeyError:
-            print("Ratelimit! Waiting for next minute to start")
-            time.sleep(60-int(datetime.datetime.now().second))
-            continue
+              print(f"Ratelimited on proxy: {proxy} - switching proxy and trying again.")
+              proxy = next(proxy_pool) # switch proxy
+              print("New Proxy: " + proxy)
+             
+              time.sleep(10)
+              continue
 
         if info.get("priceStatus", "") != "Off Sale" and info.get("collectibleItemId") is not None:
             productid = r.post("https://apis.roblox.com/marketplace-items/v1/items/details",
                    json={"itemIds": [info["collectibleItemId"]]},
-                   headers={"x-csrf-token": x_token}, cookies={".ROBLOSECURITY": cookie})
+                   headers={"x-csrf-token": x_token}, cookies={".ROBLOSECURITY": cookie}, proxies={'http':"http://"+proxy})
 
             try:
                 productid = productid.json()[0]["collectibleProductId"]
@@ -116,11 +127,11 @@ while 1:
 
             buy(info, info["collectibleItemId"], productid)
 
-    taken = time.perf_counter()-start
-    if taken < cooldown:
-        time.sleep(cooldown-taken)
+    # taken = time.perf_counter()-start
+    # if taken < cooldown:
+    #     time.sleep(cooldown-taken)
 
     os.system("cls")
-    print("Check done.\n"
-          f"Time taken: {round(time.perf_counter()-start, 3)}\n"
-          f"Ideal time: {round(cooldown, 3)}")
+    print(
+          f"Active: {round(time.perf_counter()-start, 3)}\n"
+          )
