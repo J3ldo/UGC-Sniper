f5ff216ae0f6aa0fe7334749d35de8ec57df9cf5
max <93793772+maxhithere@users.noreply.github.com>
2023-04-12 20:53:15 -0500

    gui + session update

--- main.py
+++ main.py
@@ -6,13 +6,15 @@ import time
 import datetime
 from itertools import cycle
 import colored
-from colored import fore
+from colored import fore, back, style
 import discord_webhook
 from discord_webhook import DiscordWebhook, DiscordEmbed
+import asyncio 
+from aiohttp import ClientSession, ClientResponseError
+# ^ thinking abt adding
 
-
-webhook = DiscordWebhook(url='your webhook url')
-
+webhook = DiscordWebhook(url='')
+s = requests.Session()
 
 with open("limiteds.txt", "r") as f:
     limiteds = f.read().replace(" ", "").split(",")
@@ -28,21 +30,40 @@ with open("proxies.txt", "r") as f:
 
 user_id = r.get("https://users.roblox.com/v1/users/authenticated", cookies={".ROBLOSECURITY": cookie}, proxies={'http':"http://"+proxy}).json()["id"]
 x_token = ""
+x_lims = 0
+x_proxy = 0
+stats = f"{fore.GREEN}Sniping"
+
+def run(speed, color, proxy):
+    os.system("cls")
+    print(
+        f"{fore.WHITE}[Time: {fore.YELLOW}{round(time.perf_counter()-start, 3)}{fore.WHITE}]\n  → {fore.WHITE}[Speed: {fore.YELLOW}{speed}{fore.WHITE}]\n{fore.WHITE}[Bought UGCs: {fore.GREEN}{x_lims}{fore.WHITE}]\n{fore.WHITE}[Changed Proxies: {fore.RED}{x_proxy}{fore.WHITE}]\n  → {fore.WHITE}[Current Proxy: {fore.YELLOW}{proxy}{fore.WHITE}]\n{fore.WHITE}[Status: {fore.YELLOW}{stats}{fore.WHITE}]"
+        )
+def statusCMD(got):
+    global stats
+    stats = got
+def usedProxies(proxy):
+    global x_proxy
+    x_proxy = x_proxy + 1
+    limitedProxy = proxy
+    statusCMD(f"{fore.YELLOW}Changing Proxy")
+    run("N/A", fore.YELLOW, limitedProxy)
+def boughtLim():
+    global x_lims
+    x_lims = x_lims + 1
 def get_x_token():
     global x_token
-
     x_token = r.post("https://auth.roblox.com/v2/logout",
                      cookies={".ROBLOSECURITY": cookie}, proxies={'http':"http://"+proxy}).headers["x-csrf-token"]
     
 
     while 1:
-        # Gets the x_token every 4 minutes.
         x_token = r.post("https://auth.roblox.com/v2/logout",
                          cookies={".ROBLOSECURITY": cookie}, proxies={'http':"http://"+proxy}).headers["x-csrf-token"]
         time.sleep(248)
 
 
-def buy(json, itemid, productid, prox):
+def buy(json, itemid, productid, prox, session):
     print(fore.GREEN + "BUYING LIMITED: " + productid)
     
 
@@ -59,44 +80,50 @@ def buy(json, itemid, productid, prox):
     }
 
     while 1:
-        data["idempotencyKey"] = str(uuid.uuid4())
-        bought = r.post(f"https://apis.roblox.com/marketplace-sales/v1/item/{itemid}/purchase-item", json=data,
-            headers={"x-csrf-token": x_token}, cookies={".ROBLOSECURITY": cookie}, proxies={'http':"http://"+prox})
-
-        if bought.reason == "Too Many Requests":
-            print(fore.YELLOW + "Ran into a ratelimit, switching proxy and trying again.")
-            proxy = next(proxy_pool) # switch proxy
-            print("Proxy: " + proxy)
-            time.sleep(0.5)
-            continue
-
         try:
-            bought = bought.json()
+            data["idempotencyKey"] = str(uuid.uuid4())
+            bought = session.post(f"https://apis.roblox.com/marketplace-sales/v1/item/{itemid}/purchase-item", json=data,
+                headers={"x-csrf-token": x_token}, cookies={".ROBLOSECURITY": cookie}, proxies={'http':"http://"+prox})
+
+            if bought.reason == "Too Many Requests":
+              
+                proxy = next(proxy_pool) # switch proxy
+                time.sleep(0.5)
+                usedProxies(proxy)
+                continue
+
+            try:
+                bought = bought.json()
+            except:
+                print(bought.reason)
+                print(fore.YELLOW +"Json decoder error whilst trying to buy item.")
+                continue
+
+            if not bought["purchased"]:
+                print(fore.RED + f"Failed buying the limited, trying again.. Info: {bought} - {data}")
+            else:
+                print(fore.GREEN + f"Successfully bought the limited! Info: {bought} - {data}")
+                embed = DiscordEmbed(title='Purchased Limited', description=f'{bought} - {data}', color='00FF00') 
+                webhook.add_embed(embed)
+                webhook.execute()
+                boughtLim()
+            try:
+                info = session.post("https://catalog.roblox.com/v1/catalog/items/details",
+                            json={"items": [{"itemType": "Asset", "id": int(limited)}]},
+                            headers={"x-csrf-token": x_token}, cookies={".ROBLOSECURITY": cookie}, proxies={'http':"http://"+prox})
+            except:
+                print(fore.RED + 'ERROR CAUGHT')
+            try:
+                left = info.json()["data"][0]["unitsAvailableForConsumption"] 
+            except:
+                print(fore.RED + f"Failed getting stock. Full log: {info.text} - {info.reason}")
+                left = 0
+
+            if left == 0:
+                print(fore.RED + "Couldn't buy the limited in time")
+                return
         except:
-            print(bought.reason)
-            print(fore.YELLOW +"Json decoder error whilst trying to buy item.")
-            continue
-
-        if not bought["purchased"]:
-            print(fore.RED + f"Failed buying the limited, trying again.. Info: {bought} - {data}")
-        else:
-            print(fore.GREEN + f"Successfully bought the limited! Info: {bought} - {data}")
-            embed = DiscordEmbed(title='Purchased Limited', description=f'{bought} - {data}', color='00FF00') 
-            webhook.add_embed(embed)
-            webhook.execute()
-
-        info = r.post("https://catalog.roblox.com/v1/catalog/items/details",
-                      json={"items": [{"itemType": "Asset", "id": int(limited)}]},
-                      headers={"x-csrf-token": x_token}, cookies={".ROBLOSECURITY": cookie}, proxies={'http':"http://"+prox})
-        try:
-            left = info.json()["data"][0]["unitsAvailableForConsumption"]
-        except:
-            print(fore.RED + f"Failed getting stock. Full log: {info.text} - {info.reason}")
-            left = 0
-
-        if left == 0:
-            print(fore.RED + "Couldn't buy the limited in time. Better luck next time.")
-            return
+            print(fore.RED + 'ERROR CAUGHT')
 
 
 # Get collectible and product id for all the limiteds.
@@ -105,9 +132,6 @@ Thread(target=get_x_token).start()
 print("Starting Sniper")
 while x_token == "":
     time.sleep(0.01)
-
-# https://apis.roblox.com/marketplace-items/v1/items/details
-# https://catalog.roblox.com/v1/catalog/items/details
 
 cooldown = 60/(39/len(limiteds))
 while 1:
@@ -118,12 +142,11 @@ while 1:
             info = r.post("https://catalog.roblox.com/v1/catalog/items/details",
                            json={"items": [{"itemType": "Asset", "id": int(limited)}]},
                            headers={"x-csrf-token": x_token}, cookies={".ROBLOSECURITY": cookie}, proxies={'http':"http://"+proxy}).json()["data"][0]
-        except KeyError:
-              print(fore.YELLOW + f"Ratelimited on proxy: {proxy} - switching proxy and trying again.")
-              proxy = next(proxy_pool) # switch proxy
-              print("New Proxy: " + proxy)
+        except:
              
-              time.sleep(10)
+              proxy = next(proxy_pool) # switch proxy
+              usedProxies(proxy)
+              time.sleep(5)
               continue
 
         if info.get("priceStatus", "") != "Off Sale" and info.get("collectibleItemId") is not None:
@@ -137,19 +160,13 @@ while 1:
                 print(fore.RED + f"Something went wrong whilst getting the product id Logs - {productid.text} - {productid.reason}")
                 continue
 
-            buy(info, info["collectibleItemId"], productid, proxy)
+            buy(info, info["collectibleItemId"], productid, proxy, s)
 
     taken = time.perf_counter()-start
-    print(fore.GREEN + "Start: " + str(start))
-    print(fore.GREEN + "Taken: " + str(taken))
-    print(fore.GREEN + "Cooldown: " + str(cooldown))
+    stats = f"{fore.GREEN}Running"
     if taken < cooldown:
         settle = cooldown-taken-0.7 # REMOVE -0.7 IF YOU WANT SLOWER RATES 1-1.5 BUT NO RATELIMITS
         if settle < 0:
             settle = 0.3
         time.sleep(settle) # better wait time
-
-    os.system("cls")
-    print(
-          f"Time: {round(time.perf_counter()-start, 3)}\n"
-          )
+    run(str(taken), fore.GREEN, proxy)
