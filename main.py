27f2d742e666b702766050881ebbed62c129166d
max <93793772+maxhithere@users.noreply.github.com>
2023-05-12 22:47:27 -0400

    bot support

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 # Made by:
 #   Original code, updates, and owner: Jeldo#9587
 #   Proxy support, fork, better UI, and developer: ! max#7948
+
+from extensions.base.bot import Bot
+
 VERSION = "2.5.0"
 
 try:
@@ -892,6 +895,11 @@ class UGCSniper:  # OMG guys he stole this from xolo!!
 
 
     async def main(self):
+        
+        if conf.get("bot enabled", False) is True and conf.get("bot token", "") != "":
+            bot = Bot(self)
+            asyncio.create_task(bot.start_bot())
+            
         asyncio.create_task(self.get_token())
 
         await self.wait()
